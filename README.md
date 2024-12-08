a238cf12be01d49f23f9e404e9d3c1c9f0d1025f
Neo <wtzd8645@gmail.com>
2024-12-08 15:26:22 +0800

    feat: Rename branch to ref_name and support both tags and branches

--- README.md
+++ README.md
@@ -3,15 +3,16 @@ Used for synchronizing a Git repository. Supports cross-platform.
 This action checks the status of the local Git repository and determines the appropriate steps to update it to match the latest status of the remote repository.
 
 ### Requirements
-- **Git**: Git must be installed and accessible from the command line.
+- **Git**: Git 2.23 or higher must be installed and accessible from the command line.
 - **Git-LFS**: If use_lfs is enabled, Git Large File Storage (LFS) must be installed.
 
 ### Inputs
+- **repository_path (Optional)**: The path to the repository to sync. Defaults to the current workspace.
 - **git_server (Optional)**: The Git server to sync. Defaults to github.com.
 - **use_ssh (Optional)**: Use SSH for Git authentication. Defaults to false.
 - **personal_access_token (Optional)**: Personal Access Token (PAT) for Git authentication. Defaults to github.token.
 - **repository (Optional)**: The repository to sync. Defaults to github.repository.
-- **branch (Optional)**: The branch to sync. Defaults to github.ref_name.
+- **ref_name (Optional)**: The branch or tag to sync. Defaults to github.ref_name.
 - **deep_clean (Optional)**: If true, uses the --ffdx flag to remove all untracked and ignored files; otherwise, uses -ffd to remove only untracked files. Defaults to false.
 - **use_lfs (Optional)**: Enable Git LFS for large file support. Defaults to false.
 
--- sync-git-repo.js
+++ sync-git-repo.js
@@ -2,63 +2,61 @@ import { execSync } from 'child_process';
 import { existsSync } from 'fs';
 import { join } from 'path';
 
-const gitServer = process.env.git_server || 'github.com';
-const repository = process.env.repository;
-const branch = process.env.branch;
-const cleanArgs = process.env.deep_clean === 'true' ? '-ffdx' : '-ffd';
-const useLfs = process.env.use_lfs === 'true';
-
-try {
-  const gitUrl = getGitUrl(gitServer, repository);
-  console.log(`Sync Git repository. Server: ${gitServer}, Repository: ${repository}, Branch: ${branch}`);
-  if (!existsSync(join(process.cwd(), '.git'))) {
-    console.log("Cloning repository.");
-    execCmd(`git clone --branch "${branch}" --single-branch "${gitUrl}" .`);
-  } else {
-    console.log("Cleaning working directory.");
-    execCmd(`git clean ${cleanArgs}`);
-    execCmd("git reset --hard");
-
-    console.log("Fetching latest changes from remote.");
-    execCmd(`git fetch --prune "${gitUrl}" "refs/heads/${branch}:refs/remotes/origin/${branch}" -f`);
-
-    try {
-      console.log("Checking out the branch.");
-      execCmd(`git checkout "${branch}"`);
-    } catch {
-      console.log("Creating new tracking branch.");
-      execCmd(`git checkout --track "origin/${branch}"`);
-    }
-
-    console.log("Syncing with remote branch.");
-    execCmd(`git reset --hard "origin/${branch}"`);
-  }
-
-  console.log("Cleaning submodules.");
-  execCmd(`git submodule foreach --recursive git clean ${cleanArgs}`);
-  execCmd("git submodule foreach --recursive git reset --hard");
-
-  console.log("Updating submodules.");
-  execCmd("git submodule sync");
-  execCmd("git submodule update --init --recursive");
-
-  if (useLfs) {
-    console.log("Ensuring LFS is installed.");
-    execCmd("git lfs install");
-
-    console.log("Syncing LFS files.");
-    execCmd("git lfs pull");
-    execCmd("git submodule foreach --recursive git lfs pull");
-  }
-} catch (error) {
-  console.error(error.message);
-  process.exit(1);
-}
-
-function getGitUrl(gitServer, repository) {
+function main() {
+  const repositoryPath = process.env.repository_path;
+  const gitServer = process.env.git_server || 'github.com';
+  const repository = process.env.repository;
   const useSsh = process.env.use_ssh === 'true';
   const pat = process.env.personal_access_token;
+  const refName = process.env.ref_name;
+  const cleanArgs = process.env.deep_clean === 'true' ? '-ffdx' : '-ffd';
+  const useLfs = process.env.use_lfs === 'true';
 
+  try {
+    repositoryPath && process.chdir(repositoryPath);
+
+    const gitUrl = getGitUrl(gitServer, repository, useSsh, pat);
+    if (!existsSync(join(process.cwd(), '.git'))) {
+      console.log("Cloning repository.");
+      execCmd(`git clone --no-tags --single-branch "${gitUrl}" .`);
+    }
+
+    console.log(`Fetching latest changes from remote. RefName: ${refName}`);
+    const isTagRef = execCmdAndCapture(`git ls-remote --tags "${gitUrl}" "${refName}"`).trim() !== '';
+    execCmd(`git fetch -f --prune --prune-tags "${gitUrl}" "${getFetchRefSpec(isTagRef, refName)}"`);
+
+    console.log("Cleaning working directory.");
+    execCmd(`git clean ${cleanArgs}`);
+
+    console.log("Checking out the branch.");
+    execCmd(`git switch -f ${isTagRef ? '--detach ' : ''}"${refName}"`);
+
+    console.log("Syncing with remote branch.");
+    execCmd(`git reset --hard "${getResetPathSpec(isTagRef, refName)}"`);
+
+    console.log("Cleaning submodules.");
+    execCmd(`git submodule foreach --recursive git clean ${cleanArgs}`);
+    execCmd("git submodule foreach --recursive git reset --hard");
+
+    console.log("Updating submodules.");
+    execCmd("git submodule sync");
+    execCmd("git submodule update --init --recursive");
+
+    if (useLfs) {
+      console.log("Ensuring LFS is installed.");
+      execCmd("git lfs install");
+
+      console.log("Syncing LFS files.");
+      execCmd("git lfs pull");
+      execCmd("git submodule foreach --recursive git lfs pull");
+    }
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+}
+
+function getGitUrl(gitServer, repository, useSsh, pat) {
   if (useSsh) {
     return `git@${gitServer}:${repository}.git`;
   } else if (pat) {
@@ -68,6 +66,20 @@ function getGitUrl(gitServer, repository) {
   }
 }
 
+function getFetchRefSpec(isTag, refName) {
+  return isTag ? `refs/tags/${refName}:refs/tags/${refName}` : `refs/heads/${refName}:refs/remotes/origin/${refName}`;
+}
+
+function getResetPathSpec(isTag, refName) {
+  return isTag ? refName : `origin/${refName}`;
+}
+
 function execCmd(cmd) {
   execSync(cmd, { stdio: 'inherit' });
 }
+
+function execCmdAndCapture(cmd) {
+  return execSync(cmd, { encoding: 'utf8', stdio: 'pipe' });
+}
+
+main();
